c558eb7031b9640b73a4e4caa11782da3734ef18
joao-arthur <joao.lothamer@gmail.com>
2025-05-04 14:28:41 -0300

    test(serialize): refactor locale tests and coverage

--- src/serialize/locale.rs
+++ src/serialize/locale.rs
@@ -38,66 +38,99 @@ mod tests {
     use super::{SchemaErrLocale, to_schema_localized_err};
 
     #[test]
-    fn araucaria_schema_localized_arr_to_schema_localized_err() {
+    fn araucaria_schema_localized_arr_to_schema_localized_err_validation() {
+        let araucaria_err = araucaria::locale::SchemaErrLocale::from(["required".to_string(), "str".to_string()]);
+        let err = SchemaErrLocale::Validation(vec!["required".to_string(), "str".into()]);
+        assert_eq!(to_schema_localized_err(araucaria_err), err);
+    }
+
+    #[test]
+    fn araucaria_schema_localized_arr_to_schema_localized_err_arr() {
+        let araucaria_err = araucaria::locale::SchemaErrLocale::from([
+            araucaria::locale::SchemaErrLocale::from([
+                araucaria::locale::SchemaErrLocale::from(["required".to_string(), "str".to_string()]),
+                araucaria::locale::SchemaErrLocale::from(["required".to_string(), "str".to_string()]),
+            ]),
+            araucaria::locale::SchemaErrLocale::from([
+                araucaria::locale::SchemaErrLocale::from(["str".to_string()]),
+                araucaria::locale::SchemaErrLocale::from(["str".to_string()]),
+            ]),
+        ]);
+        let err = SchemaErrLocale::Arr(vec![
+            SchemaErrLocale::Arr(vec![
+                SchemaErrLocale::Validation(vec!["required".to_string(), "str".to_string()]),
+                SchemaErrLocale::Validation(vec!["required".to_string(), "str".to_string()]),
+            ]),
+            SchemaErrLocale::Arr(vec![SchemaErrLocale::Validation(vec!["str".to_string()]), SchemaErrLocale::Validation(vec!["str".to_string()])]),
+        ]);
+        assert_eq!(to_schema_localized_err(araucaria_err), err);
+    }
+
+    #[test]
+    fn araucaria_schema_localized_arr_to_schema_localized_err_obj() {
         let araucaria_err = araucaria::locale::SchemaErrLocale::Obj(BTreeMap::from([
-            ("name".into(), araucaria::locale::SchemaErrLocale::from(["Deve ser uma string".to_string()])),
-            ("birthdate".into(), araucaria::locale::SchemaErrLocale::from(["Deve ser uma string".to_string()])),
-            ("bands".into(), araucaria::locale::SchemaErrLocale::from(["Deve ser uma string".to_string()])),
+            ("name".into(), araucaria::locale::SchemaErrLocale::from(["str".to_string()])),
+            ("birthdate".into(), araucaria::locale::SchemaErrLocale::from(["str".to_string()])),
+            ("bands".into(), araucaria::locale::SchemaErrLocale::from(["str".to_string()])),
         ]));
         let err = SchemaErrLocale::Obj(BTreeMap::from([
-            ("name".into(), SchemaErrLocale::Validation(vec!["Deve ser uma string".into()])),
-            ("birthdate".into(), SchemaErrLocale::Validation(vec!["Deve ser uma string".into()])),
-            ("bands".into(), SchemaErrLocale::Validation(vec!["Deve ser uma string".into()])),
+            ("name".into(), SchemaErrLocale::Validation(vec!["str".into()])),
+            ("birthdate".into(), SchemaErrLocale::Validation(vec!["str".into()])),
+            ("bands".into(), SchemaErrLocale::Validation(vec!["str".into()])),
         ]));
         assert_eq!(to_schema_localized_err(araucaria_err), err);
     }
 
     #[test]
+    fn serialize_schema_localized_err_validation() {
+        let validation_name = SchemaErrLocale::Validation(vec!["str".into(), r#"== "Paul McCartney""#.into()]);
+        let validation_birthdate = SchemaErrLocale::Validation(vec!["str".into(), r#"== "1942-06-18""#.into()]);
+        let validation_alive = SchemaErrLocale::Validation(vec!["bool".into(), "== true".into()]);
+        let validation_band = SchemaErrLocale::Validation(vec!["str".into(), r#"== "The Beatles""#.into()]);
+
+        assert_eq!(serde_json::to_string(&validation_name).unwrap(), r#"["str","== \"Paul McCartney\""]"#.to_string());
+        assert_eq!(serde_json::to_string(&validation_birthdate).unwrap(), r#"["str","== \"1942-06-18\""]"#.to_string());
+        assert_eq!(serde_json::to_string(&validation_alive).unwrap(), r#"["bool","== true"]"#.to_string());
+        assert_eq!(serde_json::to_string(&validation_band).unwrap(), r#"["str","== \"The Beatles\""]"#.to_string());
+    }
+
+    #[test]
+    fn serialize_schema_localized_err_arr() {
+        let err = SchemaErrLocale::Arr(vec![
+            SchemaErrLocale::Obj(BTreeMap::from([
+                ("name".into(), SchemaErrLocale::Validation(vec!["str".into()])),
+                ("birthdate".into(), SchemaErrLocale::Validation(vec!["str".into()])),
+            ])),
+            SchemaErrLocale::Arr(vec![SchemaErrLocale::Validation(vec!["bool".into()]), SchemaErrLocale::Validation(vec!["u64".into()])]),
+        ]);
+        assert_eq!(serde_json::to_string(&err).unwrap(), r#"[{"birthdate":["str"],"name":["str"]},[["bool"],["u64"]]]"#.to_string());
+    }
+
+    #[test]
     fn serialize_schema_localized_err_obj() {
-        let err_name = SchemaErrLocale::Obj(BTreeMap::from([(
-            "name".into(),
-            SchemaErrLocale::Validation(vec!["É obrigatório".into(), "Deve ser uma string".into(), r#"Deve ser igual a "Paul McCartney""#.into()]),
-        )]));
-        let err_birthdate = SchemaErrLocale::Obj(BTreeMap::from([(
-            "birthdate".into(),
-            SchemaErrLocale::Validation(vec!["É obrigatório".into(), "Deve ser uma string".into(), r#"Deve ser igual a "1942-06-18""#.into()]),
-        )]));
-        let err_alive = SchemaErrLocale::Obj(BTreeMap::from([(
-            "alive".into(),
-            SchemaErrLocale::Validation(vec!["É obrigatório".into(), "Deve ser um booleano".into(), "Deve ser igual a true".into()]),
-        )]));
-        let err_bands = SchemaErrLocale::Obj(BTreeMap::from([(
-            "bands".into(),
-            SchemaErrLocale::Validation(vec!["É obrigatório".into(), "Deve ser uma string".into(), r#"Deve ser igual a "The Beatles""#.into()]),
-        )]));
-        assert_eq!(
-            serde_json::to_string(&err_name).unwrap(),
-            r#"{"name":["É obrigatório","Deve ser uma string","Deve ser igual a \"Paul McCartney\""]}"#.to_string()
-        );
-        assert_eq!(
-            serde_json::to_string(&err_birthdate).unwrap(),
-            r#"{"birthdate":["É obrigatório","Deve ser uma string","Deve ser igual a \"1942-06-18\""]}"#.to_string()
-        );
-        assert_eq!(
-            serde_json::to_string(&err_alive).unwrap(),
-            r#"{"alive":["É obrigatório","Deve ser um booleano","Deve ser igual a true"]}"#.to_string()
-        );
-        assert_eq!(
-            serde_json::to_string(&err_bands).unwrap(),
-            r#"{"bands":["É obrigatório","Deve ser uma string","Deve ser igual a \"The Beatles\""]}"#.to_string()
-        );
+        let validation_name = SchemaErrLocale::Validation(vec!["str".into(), r#"== "Paul McCartney""#.into()]);
+        let validation_birthdate = SchemaErrLocale::Validation(vec!["str".into(), r#"== "1942-06-18""#.into()]);
+        let validation_alive = SchemaErrLocale::Validation(vec!["bool".into(), "== true".into()]);
+        let validation_band = SchemaErrLocale::Validation(vec!["str".into(), r#"== "The Beatles""#.into()]);
+
+        let err_name = SchemaErrLocale::Obj(BTreeMap::from([("name".into(), validation_name)]));
+        let err_birthdate = SchemaErrLocale::Obj(BTreeMap::from([("birthdate".into(), validation_birthdate)]));
+        let err_alive = SchemaErrLocale::Obj(BTreeMap::from([("alive".into(), validation_alive)]));
+        let err_band = SchemaErrLocale::Obj(BTreeMap::from([("band".into(), validation_band)]));
+
+        assert_eq!(serde_json::to_string(&err_name).unwrap(), r#"{"name":["str","== \"Paul McCartney\""]}"#.to_string());
+        assert_eq!(serde_json::to_string(&err_birthdate).unwrap(), r#"{"birthdate":["str","== \"1942-06-18\""]}"#.to_string());
+        assert_eq!(serde_json::to_string(&err_alive).unwrap(), r#"{"alive":["bool","== true"]}"#.to_string());
+        assert_eq!(serde_json::to_string(&err_band).unwrap(), r#"{"band":["str","== \"The Beatles\""]}"#.to_string());
     }
 
     #[test]
     fn serialize_schema_localized_err_obj_order() {
         let err = SchemaErrLocale::Obj(BTreeMap::from([
-            ("name".into(), SchemaErrLocale::Validation(vec!["Deve ser uma string".into()])),
-            ("birthdate".into(), SchemaErrLocale::Validation(vec!["Deve ser uma string".into()])),
-            ("bands".into(), SchemaErrLocale::Validation(vec!["Deve ser uma string".into()])),
+            ("name".into(), SchemaErrLocale::Validation(vec!["str".into()])),
+            ("birthdate".into(), SchemaErrLocale::Validation(vec!["str".into()])),
+            ("bands".into(), SchemaErrLocale::Validation(vec!["str".into()])),
         ]));
-        assert_eq!(
-            serde_json::to_string(&err).unwrap(),
-            r#"{"bands":["Deve ser uma string"],"birthdate":["Deve ser uma string"],"name":["Deve ser uma string"]}"#.to_string()
-        );
+        assert_eq!(serde_json::to_string(&err).unwrap(), r#"{"bands":["str"],"birthdate":["str"],"name":["str"]}"#.to_string());
     }
 }
